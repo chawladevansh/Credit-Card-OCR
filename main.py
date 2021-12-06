1972f177e64f9a3dd24460ec50c6731d41c314cb
Devansh <devansh@vt.edu>
2021-12-05 22:21:51 -0500

    Added Video Capture

--- main.py
+++ main.py
@@ -44,31 +44,96 @@ def processRefImage(image):
 img = cv2.imread('ocr_ref.png')
 image, contours, digits = processRefImage(img)
 
-# Credit Card Image Processing
-card = cv2.imread('test1.png')
-card = resize_image(card, width = 300)
-card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
 
-kernel93 = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
-# Tophat morphological transformation
-tophat = cv2.morphologyEx(card_gray, cv2.MORPH_TOPHAT, kernel93)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-# Sobel Filter gradient smoothening over x Axis
-sobel = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
-sobel = np.absolute(sobel)
+while True:
+    # Capture frame-by-frame
+    ret, card = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+# # Credit Card Image Processing
+# card = cv2.imread('test1.png')
+    card = resize_image(card, width = 300)
+    card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
 
-(minThresh, maxThresh) = (np.min(sobel), np.max(sobel))
+    kernel93 = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+    # Tophat morphological transformation
+    tophat = cv2.morphologyEx(card_gray, cv2.MORPH_TOPHAT, kernel93)
 
-sobel = (255 * ((sobel - minThresh) / (maxThresh - minThresh)))
-sobel = sobel.astype("uint8")
+    # Sobel Filter gradient smoothening over x Axis
+    sobel = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+    sobel = np.absolute(sobel)
 
-kernel5 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    (minThresh, maxThresh) = (np.min(sobel), np.max(sobel))
 
-sobel = cv2.morphologyEx(sobel, cv2.MORPH_CLOSE, kernel93)
-sobel_thresh = cv2.threshold(sobel, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-sobel_thresh = cv2.morphologyEx(sobel_thresh, cv2.MORPH_CLOSE, kernel5)
+    sobel = (255 * ((sobel - minThresh) / (maxThresh - minThresh)))
+    sobel = sobel.astype("uint8")
 
-# Credit Card Image Contours
-card_contours = cv2.findContours(sobel_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-card_contours = imutils.grab_contours(card_contours)
+    kernel5 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 
+    sobel = cv2.morphologyEx(sobel, cv2.MORPH_CLOSE, kernel93)
+    sobel_thresh = cv2.threshold(sobel, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    sobel_thresh = cv2.morphologyEx(sobel_thresh, cv2.MORPH_CLOSE, kernel5)
+
+    # Credit Card Image Contours
+    card_contours = cv2.findContours(sobel_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    card_contours = imutils.grab_contours(card_contours)
+    digit_locations = []
+
+    for (digit, contour) in enumerate(card_contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        aspect_ratio = w / float(h)
+        if aspect_ratio > 2.5 and aspect_ratio < 4.0:
+            if(w > 40 and w < 55) and (h > 10 and h < 20):
+                digit_locations.append((x,y,w,h))
+
+    digit_locations = sorted(digit_locations, key=lambda x:x[0])
+
+
+    #####
+    output = []
+
+    for (i, (X, Y, W, H)) in enumerate(digit_locations):
+        groupOp = []
+        group = card_gray[Y - 5:Y + H + 5, X - 5:X + W + 5]
+        group = cv2.threshold(group, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        digitContours = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        digitContours = imutils.grab_contours(digitContours)
+        #digitContours = contours.sort_contours(digitContours, method="left-to-right")[0]
+
+        for contour in digitContours:
+            (x,y,w,h) = cv2.boundingRect(contour)
+            point = group[y:y+h, x:x+w]
+            point = cv2.resize(point, (57,88)) ##########
+
+            matches = []
+            for(digit, points) in digits.items():
+                # Template matching
+                output = cv2.matchTemplate(point, points, cv2.TM_CCOEFF)
+                (_, match, _, _) = cv2.minMaxLoc(output)
+                matches.append(match)
+
+                groupOp.append(str(np.argmax(matches)))
+
+                cv2.rectangle(card, (X - 5, Y - 5), (X + W + 5, Y + H + 5), (0, 0, 255), 2)
+                cv2.putText(card, "".join(groupOp), (X, Y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+                #output.extend(groupOp)
+
+    # cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
+    # plt.imshow(cv2.cvtColor(card, cv2.COLOR_BGR2RGB))
+    # plt.title('Image'); plt.show()
+
+    cv2.imshow('frame', card)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
